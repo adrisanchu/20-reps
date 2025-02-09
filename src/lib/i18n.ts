3444204c9c8633d1eaa91bb8adf8de31bed6a953
adrisanchu <adrian.sanchez@constant-urban.eu>
2025-02-10 00:04:14 +0100

    `/settings` endpoint setup + to visualize each of the sport colors

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,3 +1,11 @@
-import * as runtime from '$lib/paraglide/runtime';
-import { createI18n } from '@inlang/paraglide-sveltekit';
-export const i18n = createI18n(runtime);
+import * as runtime from "$lib/paraglide/runtime";
+import { createI18n } from "@inlang/paraglide-sveltekit";
+export const i18n = createI18n(runtime, {
+  pathnames: {
+    "/settings": {
+      en: "/settings",
+      es: "/ajustes",
+      fr: "/parametres",
+    },
+  },
+});
---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+  import * as m from '$lib/paraglide/messages.js';
+  import { Badge } from '$lib/components/ui/badge/index.js';
+  import type { StravaSportType } from '$lib/types';
+
+  let activities: {
+    id: number;
+    name: string;
+    sport_type: keyof typeof StravaSportType;
+  }[] = [
+    {
+      id: 1,
+      name: 'Activity 1',
+      sport_type: 'Ride',
+    },
+    {
+      id: 2,
+      name: 'Activity 2',
+      sport_type: 'GravelRide',
+    },
+    {
+      id: 3,
+      name: 'Activity 3',
+      sport_type: 'WeightTraining',
+    },
+    {
+      id: 4,
+      name: 'Activity 4',
+      sport_type: 'AlpineSki',
+    },
+
+    {
+      id: 5,
+      name: 'Activity 5',
+      sport_type: 'Kitesurf',
+    },
+    {
+      id: 6,
+      name: 'Activity 6',
+      sport_type: 'Swim',
+    },
+    {
+      id: 7,
+      name: 'Activity 7',
+      sport_type: 'Yoga',
+    },
+  ];
+
+  let activityColors: { [key in keyof typeof StravaSportType]: string } = {
+    Ride: 'bg-yellow-400',
+    GravelRide: 'bg-yellow-500',
+    WeightTraining: 'bg-orange-500',
+    AlpineSki: 'bg-sky-500',
+    Kitesurf: 'bg-blue-600',
+    Swim: 'bg-cyan-500',
+    Yoga: 'bg-pink-500',
+  };
+</script>
+
+<div class="min-h-screen">
+  <main class="container mx-auto px-4 py-10">
+    <h1 class="text-5xl font-bold text-center mb-12">{m.settings_title()}</h1>
+    <h2 class="text-3xl font-bold">{m.settings_sports_title()}</h2>
+    <ul class="mt-4 space-y-4">
+      {#each activities as activity (activity.id)}
+        <li class="flex flex-col b-4">
+          <div class="flex justify-between">
+            <Badge
+              variant="secondary"
+              class={activityColors[activity.sport_type]}
+              >{activity.sport_type}</Badge
+            >
+          </div>
+        </li>
+      {/each}
+    </ul>
+  </main>
+</div>
