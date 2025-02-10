d7e775f5b990100c6d0e4aee63439d1ca44bf3c0
adrisanchu <adrian.sanchez@constant-urban.eu>
2025-02-10 18:08:35 +0100

    db binding + new serverless function to store data in cloudflare D1 + a button for testing

---
+++ src/routes/d1-test/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  let activities = [
+    {
+      id: 13238591234,
+      name: 'Test - Primera del año',
+      start_date: '2025-01-01T12:02:34Z',
+      sport_type: 'GravelRide',
+      distance: 19134.2,
+      moving_time: 4388,
+      elapsed_time: 7282,
+      extra_data: {
+        total_elevation_gain: 244,
+      },
+    },
+  ];
+
+  async function saveActivities() {
+    try {
+      const res = await fetch('/api/save-activities', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ activities }),
+      });
+
+      if (!res.ok) throw new Error(await res.text());
+      alert('Activities saved successfully!');
+    } catch (error) {
+      console.error('Error saving activities:', error);
+      alert('Failed to save activities.');
+    }
+  }
+</script>
+
+<button on:click={saveActivities}>Save Activities</button>
