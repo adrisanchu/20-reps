59f825ff0e6a11c19cd15c95557f6c72fb0490b1
adrisanchu <adrian.sanchez@constant-urban.eu>
2025-02-11 20:12:52 +0100

    added dummy endpoint to test D1 both locally and in prod

---
+++ src/routes/activities/+page.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+  import { Button } from '$lib/components/ui/button/index.js';
+  import ActivityList from '$lib/components/ActivityList.svelte';
+
+  import type { Activity } from '$lib/types';
+
+  let activities = [
+    {
+      id: 13238591234,
+      name: 'Primera del año',
+      start_date: '2025-01-01T12:02:34Z',
+      sport_type: 'GravelRide',
+      distance: 19134.2,
+      elapsed_time: 0,
+      moving_time: 0,
+      extra_data: {
+        moving_time: 4388,
+        elapsed_time: 7282,
+        total_elevation_gain: 244,
+      },
+    },
+    {
+      id: 13238831929,
+      name: 'W1 - 20 Reps',
+      start_date: '2025-01-01T14:38:22Z',
+      sport_type: 'WeightTraining',
+      distance: 0,
+      elapsed_time: 0,
+      moving_time: 0,
+      extra_data: {
+        moving_time: 402,
+        elapsed_time: 402,
+      },
+    },
+  ];
+
+  let activitiesFromDB: Activity[] = [];
+
+  async function saveActivities() {
+    try {
+      const res = await fetch('/api/activities', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ activities }),
+      });
+
+      if (!res.ok) throw new Error(await res.text());
+      alert('Activities saved successfully!');
+      // goto('/'); // Redirect or refresh page
+    } catch (error) {
+      console.error('Error saving activities:', error);
+      alert('Failed to save activities.');
+    }
+  }
+
+  async function getActivities() {
+    try {
+      const res = await fetch('/api/activities', {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json',
+        },
+      });
+
+      const { results } = (await res.json()) as { results: Activity[] };
+      activitiesFromDB = results;
+    } catch (error) {
+      console.error('Error getting activities:', error);
+    }
+  }
+</script>
+
+<main class="flex flex-col justify-center max-w-xl mx-auto px-4 py-10">
+  <div class="mb-4 space-x-2">
+    <Button
+  class="font-semibold text-foreground bg-sky-500"
+  onclick={saveActivities}>Save Activities</Button
+>
+<Button
+  class="font-semibold text-foreground bg-orange-600"
+  onclick={getActivities}>Get Activities</Button
+>
+  </div>
+  
+
+{#if activitiesFromDB.length > 0}
+  <ActivityList activities={activitiesFromDB}></ActivityList>
+{/if}
+</main>
+
